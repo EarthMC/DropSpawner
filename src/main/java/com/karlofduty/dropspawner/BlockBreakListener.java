c98cbbe1da7a3ec7d9856b8cc7362b1a59de13f2
Mitchell <13673076+Mishyy@users.noreply.github.com>
2022-08-10 19:37:59 +1000

    Use dependency injection, and general cleanup.

--- src/main/java/com/karlofduty/dropspawner/BlockBreakListener.java
+++ src/main/java/com/karlofduty/dropspawner/BlockBreakListener.java
@@ -1,83 +1,93 @@
 package com.karlofduty.dropspawner;
 
+import java.util.Objects;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class BlockBreakListener implements Listener
-{
-    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockBreak(BlockBreakEvent event)
-    {
-        //Abort if not a mob spawner
-        if (!event.getBlock().getType().equals(Material.SPAWNER))
-            return;
+public final class BlockBreakListener implements Listener {
 
-        if (event.getPlayer().getGameMode().equals(GameMode.CREATIVE)) {
-            DropSpawner.log(event.getPlayer().getName() + " broke a spawner but was in creative mode.");
-            return;
-        }
+	private final DropSpawner plugin;
 
-        //Abort if not the required tool
-        if(DropSpawner.config.getBoolean("require-pickaxe") && !isPickaxe(event.getPlayer().getInventory().getItemInMainHand().getType()))
-        {
-            DropSpawner.log(event.getPlayer().getName() + " broke a spawner at " + formatLocation(event.getBlock().getLocation()) + ", but did not use a pickaxe.");
-            return;
-        }
+	public BlockBreakListener(final DropSpawner plugin) {
+		this.plugin = plugin;
+	}
 
-        //Abort if not the required enchant
-        if (DropSpawner.config.getBoolean("require-silktouch") && !event.getPlayer().getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
-            DropSpawner.log(event.getPlayer().getName() + " broke a spawner at " + formatLocation(event.getBlock().getLocation()) + ", but did not use silk touch.");
-            return;
-        }
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onBlockBreak(final BlockBreakEvent event) {
+		// Abort if not a mob spawner
+		final Block block = event.getBlock();
+		if (block.getType() != Material.SPAWNER) {
+			return;
+		}
 
-        // Abort if the player does not have permission to get spawner drops
-        if(!event.getPlayer().hasPermission("dropspawner.allowdrop"))
-        {
-            DropSpawner.log(event.getPlayer().getName() + " broke a spawner at " + formatLocation(event.getBlock().getLocation()) + ", but did not have permission to drop it.");
-            return;
-        }
+		final Player player = event.getPlayer();
+		if (player.getGameMode() == GameMode.CREATIVE) {
+			plugin.getLogger().info(player.getName() + " broke a spawner but was in creative mode.");
+			return;
+		}
 
-        event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.SPAWNER, 1));
-        DropSpawner.log(event.getPlayer().getName() + " broke a spawner succesfully at " + formatLocation(event.getBlock().getLocation()) + ".");
+		// Abort if not the required tool
+		final Location location = block.getLocation();
+		final String locationStr = formatLocation(block);
+		final ItemStack itemInHand = player.getInventory().getItemInMainHand();
+		if (plugin.getConfig().getBoolean("require-pickaxe") && !isPickaxe(itemInHand.getType())) {
+			plugin.getLogger().info(player.getName() + " broke a spawner at " + locationStr + ", but did not use a pickaxe.");
+			return;
+		}
 
-        CreatureSpawner spawner = (CreatureSpawner) event.getBlock().getState();
-        EntityType spawnerType = spawner.getSpawnedType();
-        if(!spawnerType.equals(EntityType.UNKNOWN) && !spawnerType.equals(EntityType.PIG))
-        {
-            ItemStack mobEggToSpawn = new ItemStack(Material.valueOf(spawnerType + "_SPAWN_EGG"), 1);
-            event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), mobEggToSpawn);
-            DropSpawner.log(event.getPlayer().getName() + " got a spawn egg.");
-        }
+		// Abort if not the required enchant
+		if (plugin.getConfig().getBoolean("require-silktouch") && !itemInHand.containsEnchantment(Enchantment.SILK_TOUCH)) {
+			plugin.getLogger().info(player.getName() + " broke a spawner at " + locationStr + ", but did not use silk touch.");
+			return;
+		}
 
-        event.setExpToDrop(0);
-        event.setDropItems(false);
-    }
+		// Abort if the player does not have permission to get spawner drops
+		if (!player.hasPermission("dropspawner.allowdrop")) {
+			plugin.getLogger().info(player.getName() + " broke a spawner at " + locationStr + ", but did not have permission to drop it.");
+			return;
+		}
 
-    private boolean isPickaxe(Material material) {
-        switch(material) {
-            case WOODEN_PICKAXE:
-            case STONE_PICKAXE:
-            case IRON_PICKAXE:
-            case GOLDEN_PICKAXE:
-            case DIAMOND_PICKAXE:
-            case NETHERITE_PICKAXE:
-                return true;
-            default:
-                return false;
-        }
-    }
+		block.getWorld().dropItemNaturally(location, new ItemStack(Material.SPAWNER)); // ItemStack has an amount of 1 by default.
+		plugin.getLogger().info(player.getName() + " broke a spawner successfully at " + locationStr + ".");
 
-    private String formatLocation(Location location) {
-        String world = location.getWorld() == null ? "null" : location.getWorld().getName();
-        return "{world=" + world + ", x=" + location.getBlockX() + ", y=" + location.getBlockY() + ", z=" + location.getBlockZ() + "}";
-    }
+		final EntityType spawnerType = ((CreatureSpawner) block.getState()).getSpawnedType();
+		if (spawnerType != EntityType.UNKNOWN && spawnerType != EntityType.PIG) {
+			// Use Bukkit's in-built name reference map.
+			block.getWorld().dropItemNaturally(location, new ItemStack(Objects.requireNonNull(Material.getMaterial(spawnerType + "_SPAWN_EGG"))));
+			plugin.getLogger().info(player.getName() + " got a spawn egg.");
+		}
+
+		event.setExpToDrop(0);
+		event.setDropItems(false);
+	}
+
+	private String formatLocation(final Block block) {
+		// Block's world will never be null.
+		return "{world=" + block.getWorld() + ", x=" + block.getX() + ", y=" + block.getY() + ", z=" + block.getZ() + "}";
+	}
+
+	private boolean isPickaxe(final Material material) {
+		switch (material) {
+			case WOODEN_PICKAXE:
+			case STONE_PICKAXE:
+			case IRON_PICKAXE:
+			case GOLDEN_PICKAXE:
+			case DIAMOND_PICKAXE:
+			case NETHERITE_PICKAXE:
+				return true;
+		}
+		return false;
+	}
+
 }
